1463793b7d4a419b3e48f0eb8ad72f212eb9b60d
witwint <witwint1234@gmail.com>
2023-04-09 17:21:07 +0900

    [feat]유저기본등록 스테틱 미디어/ 홈화면 설정환료

--- users/models.py
+++ users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+	
 # Create your models here.
-class User(models.Model):
+class Myuser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     userid = models.CharField(max_length=50, null=True, verbose_name="이름")
     password = models.CharField(max_length=50, null=True, verbose_name="비밀번호")
-    nickname = models.CharField(max_length=50, null=True, verbose_name="비밀번호")
-    email = models.CharField(max_length=50, null=True, verbose_name="비밀번호")
+    nickname = models.CharField(max_length=50, null=True, verbose_name="닉네임")
+    email = models.CharField(max_length=50, null=True, verbose_name="이메일")
     userimage = models.ImageField(blank=True,upload_to='userimage/',verbose_name="유저사진", null=True)
     usermemo = models.TextField(verbose_name="유저소개")
--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = "albums"
+
+app_name = "users"
 
 urlpatterns = [
-    path('main', views.main, name='main'),
-]
+    path('', views.main, name='main'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
