65c459fd63b59c68ca17050e915fb41da1e2f45d
witwint <witwint1234@gmail.com>
2023-04-24 21:33:06 +0900

    [feat] 게시판 update

--- boards/urls.py
+++ boards/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', views.board, name='board'),
     path('<int:id>',views.detail, name='detail'),
     path('create',views.create, name='create'),
+    path('update/<int:id>',views.update, name='update'),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- boards/views.py
+++ boards/views.py
@@ -28,7 +28,7 @@ def create(request):
   if request.method == "POST":
     
     user = Myuser.objects.get(id = request.user.id)
-    boardphoto = request.FILES.get("boardphto")
+    boardphoto = request.FILES.get("boardphoto")
     boardtitle = request.POST["boardtitle"]
     boardcontext = request.POST["boardcontext"]
     postcategory = request.POST["postcategory"]
@@ -46,3 +46,33 @@ def create(request):
     "categoryinfo": category,
   }
   return render(request, template_name="boards/create.html",context=context)
+
+def update(request,id):
+  board = Board.objects.get(pk = id)
+  if request.method == "POST":
+
+    clearimage = request.POST.get('clearimage')
+    boardphoto = request.FILES.get("boardphoto")
+    if boardphoto == None:
+      if clearimage == 'clearimage':
+        boardphoto = None
+      else:
+        boardphoto = board.boardimage
+    boardtitle = request.POST["boardtitle"]
+    boardcontext = request.POST["boardcontext"]
+    postcategory = request.POST["postcategory"]
+    postcategory = Boardcategory.objects.get(name = postcategory)
+    board.title = boardtitle
+    board.context = boardcontext
+    board.category = postcategory
+    board.boardimage = boardphoto
+    board.save()
+    return redirect(f"/board/{board.id}")
+
+  
+  category = Boardcategory.objects.all()
+  context = {
+    "boardinfo": board,
+    "categoryinfo": category,
+  }
+  return render(request, template_name="boards/update.html",context=context)
